747982e048b7fa06e4f10a03cbd165539b49860b
zhura <vlad.zhurakovskiy@igamingplatform.com>
2019-04-14 20:15:29 +0300

    done

---
+++ App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+      jokes: {},
+      text: [],
+      result: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.chucknorris.io/jokes/categories').then(res=>{
+      res.json().then(data => this.setState({categories: data}))
+    });
+  }
+
+
+  // getMatchesJokes(text) {
+  //   fetch(`https://api.chucknorris.io/jokes/search?query=${text}`).then(res => {
+  //     res.json().then(data => console.log(data.result[0].value))
+  //   });
+  // };
+
+
+  getCategoriesJokes(category) {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`).then(res=>{
+      res.json().then(data => this.setState({jokes: data}))
+    });
+  };
+
+
+  getMatchesJokes = (text) => {
+    fetch(`https://api.chucknorris.io/jokes/search?query=${text}`).then(res => {
+      res.json().then(data => this.setState({result: data.result}))
+    });
+  };
+
+  render() {
+    const {categories, jokes, result} = this.state;
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+          <TextInput
+              style={{width: 200, height: 40, margin: 'auto'}}
+              placeholder="Enter pls"
+              onChangeText={this.getMatchesJokes}
+          />
+
+          <View>
+            {this.state.result.map((item, index) => (
+                <Text key={index}>{item.value}</Text>
+            ))}
+          </View>
+
+
+          <View>
+            {categories.map((item, index) => (
+              <Text key={index} onPress={this.getCategoriesJokes.bind(this, item)}>
+                {item}
+              </Text>
+            ))}
+          </View>
+
+          <Text>{jokes.value}</Text>
+        </View>
+    );
+  }
+}
